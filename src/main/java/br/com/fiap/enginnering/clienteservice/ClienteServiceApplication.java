e56834c9b46238821fe775d4499a496ebe5f8bff
Wellington Barros Freitas <wfreitas@Wellingtons-MacBook-Pro.local>
2023-01-19 20:11:27 -0300

    feat: inciado cliente service

--- src/main/java/br/com/fiap/enginnering/clienteservice/ClienteServiceApplication.java
+++ src/main/java/br/com/fiap/enginnering/clienteservice/ClienteServiceApplication.java
@@ -2,12 +2,7 @@ package br.com.fiap.enginnering.clienteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
